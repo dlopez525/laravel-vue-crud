39920dc73f1daf498def7ba2bdf6f20e586d2afc
Daniel López Sánchez <dlopezs2009@hotmail.com>
2019-02-24 15:53:15 -0500

    Initial commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,22 @@
+<form method="post" v-on:submit.prevent="createKeep">
+    <div class="modal fade" id="create">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Nueva tarea</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <label for="keep">Crear Tarea</label>
+                    <input type="text" class="form-control" name="keep" v-model="newKeep">
+                    <span v-for="error in errors" class="text-danger">@{{ error }}</span>
+                </div>
+                <div class="modal-footer">
+                    <input type="submit" class="btn btn-primary" value="Guardar">
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
---
+++ resources/views/edit.blade.php
@@ -0,0 +1,22 @@
+<form method="post" v-on:submit.prevent="updateKeep(fillKeep.id)">
+    <div class="modal fade" id="edit">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Editar tarea</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <label for="keep">Editar Tarea</label>
+                    <input type="text" class="form-control" name="keep" v-model="fillKeep.keep">
+                    <span v-for="error in errors" class="text-danger">@{{ error }}</span>
+                </div>
+                <div class="modal-footer">
+                    <input type="submit" class="btn btn-primary" value="Actualizar">
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
